6154743b3db3f1e9a19fc2ac37f1361f68dddc09
danikun <steelturtle@msn.com>
2020-11-26 13:47:14 +0900

    First Commit

---
+++ sfs_server/files/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from .views import FilesListView, FileView
+
+app_name = 'files'
+
+
+urlpatterns = [
+    path('files', FilesListView.as_view(), name='files_list'),
+    path('files/<int:pk>', FileView.as_view(), name='file_detail'),
+]
+urlpatterns = format_suffix_patterns(urlpatterns)
